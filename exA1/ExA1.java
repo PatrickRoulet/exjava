9283e932afe8b1e35ff8d8a1956fb4a9b2a94b8a
PatrickRoulet <patrick.roulet@bnpparibasfortis.com>
2020-09-22 10:02:57 +0200

    Added instructors

--- exA1/ExA1.java
+++ exA1/ExA1.java
@@ -1,8 +1,12 @@
+
+import java.util.*;
+
 public class ExA1 {
     public String title;
     public int numberOfDays;
     public float pricePerDay;
     public boolean priorKnowledgeRequired;
+    public List<String> listOfInstructors = new ArrayList<String>();
 
     public static void main(String[] args) {
         ExA1 course = new ExA1();
@@ -10,11 +14,16 @@ public class ExA1 {
 	course.numberOfDays = 3;
 	course.pricePerDay = 400.0f;
 	course.priorKnowledgeRequired = false;
+	course.listOfInstructors.add("Instructor1");
+	course.listOfInstructors.add("Instructor2");
+	course.listOfInstructors.add("Instructor3");
+	
         System.out.println("Course per default created");
         System.out.println("Title : "+course.title);
         System.out.println("Number of days : "+ course.numberOfDays);
         System.out.println("Price per day : "+ course.pricePerDay);
         System.out.println("Prior knowledge required : "+ course.priorKnowledgeRequired);
+        System.out.println("Number of instructors : "+ course.listOfInstructors.size());
 
     }
 

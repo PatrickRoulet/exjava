8e2b367007d084d61e97f00fdba05a2b4e3b0fff
PatrickRoulet <patrick.roulet@bnpparibasfortis.com>
2020-09-22 09:50:21 +0200

    Course per default - display infos

--- exA1/ExA1.java
+++ exA1/ExA1.java
@@ -6,7 +6,15 @@ public class ExA1 {
 
     public static void main(String[] args) {
         ExA1 course = new ExA1();
+	course.title = "Course per default";
+	course.numberOfDays = 3;
+	course.pricePerDay = 400.0f;
+	course.priorKnowledgeRequired = false;
         System.out.println("Course per default created");
+        System.out.println("Title : "+course.title);
+        System.out.println("Number of days : "+ course.numberOfDays);
+        System.out.println("Price per day : "+ course.pricePerDay);
+        System.out.println("Prior knowledge required : "+ course.priorKnowledgeRequired);
 
     }
 

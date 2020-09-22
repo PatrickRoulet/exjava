6ca0ac2c09e2518ecd019f3c380ecce782815e05
PatrickRoulet <patrick.roulet@bnpparibasfortis.com>
2020-09-22 09:44:01 +0200

    File ExA1 created

---
+++ exA1/ExA1.java
@@ -0,0 +1,13 @@
+public class ExA1 {
+    public String title;
+    public int numberOfDays;
+    public float pricePerDay;
+    public boolean priorKnowledgeRequired;
+
+    public static void main(String[] args) {
+        ExA1 course = new ExA1();
+        System.out.println("Course per default created");
+
+    }
+
+}
